84976621bb45986a6875de01589986296e1b9b68
Yuting Liu <83076528+yeziver@users.noreply.github.com>
2023-02-16 16:49:21 -0500

    Create prime_cryptarithm.cpp

---
+++ misc/prime_cryptarithm.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(void) {
+    int n; 
+    cin >> n;
+    vector<int> d(n);
+    for (int i = 0 ; i < n; i++) {
+        cin >> d[i];
+    }
+    for (int a = 0; a < n ; a++) {
+        
+    }
+}
